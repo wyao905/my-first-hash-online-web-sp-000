c01a3bbdb28568bebb071e68d2a24f0845906104
Warren Yao <warrenyca905@gmail.com>
2019-05-22 01:10:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
-  shipping_manifeset["gun powder"] = 4
+  shipping_manifest["gun powder"] = 4
   
   shipping_manifest
 end
